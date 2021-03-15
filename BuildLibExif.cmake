de66320f7573501b0070af9bdd874c81399e6a66
Stuart MacDonald <smacdonald@2grobotics.com>
2021-03-15 05:49:28 -0400

    Try not to apply the patch multiple times
    Probably libexif should be a conan recipe, and the patch should happen
    there.

--- BuildLibExif.cmake
+++ BuildLibExif.cmake
@@ -14,11 +14,14 @@ add_definitions(-DGETTEXT_PACKAGE="libexif-12")
 configure_file("lib/config.h" "${CMAKE_CURRENT_SOURCE_DIR}/lib/libexif/config.h" COPYONLY)
 configure_file("lib/_stdint.h" "${CMAKE_CURRENT_SOURCE_DIR}/lib/libexif/libexif/_stdint.h" COPYONLY)
 
-#apply patch
-
-execute_process(COMMAND git apply lib/tiff_support.patch --ignore-space-change --ignore-whitespace --directory=lib/libexif/
+# Apply patch only once. Better would be if 'git apply' detected that the patch has already been applied, because
+# it still fails in the case where the cache is manually removed and configuration is rerun.
+if (NOT LIBEXIF_PATCHES_APPLIED MATCHES "true")
+    execute_process(COMMAND git apply lib/tiff_support.patch --ignore-space-change --ignore-whitespace --directory=lib/libexif/
                     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                     RESULT_VARIABLE GIT_PATCH_RESULT)
+    set(LIBEXIF_PATCHES_APPLIED "true" CACHE INTERNAL "" FORCE)
+endif()
 
 
 include_directories("lib/libexif/")
