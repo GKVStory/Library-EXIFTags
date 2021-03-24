bf2f3fbcb49015a5858b37d05d0fd7eed5224b9d
Ryan Wicks <ryancwicks@gmail.com>
2021-03-24 13:15:58 -0400

    Adjusted the image processing script to work with nav and depth, now tested and working.,

--- scripts/process_directory_nav.py
+++ scripts/process_directory_nav.py
@@ -3,13 +3,13 @@ import numpy as np
 import EXIFTagsLibPython as et
 import datetime as dt
 
-depth_file = Path("D:\\_Investigations\\2021-03-19-taggingImages\\depth.csv")
-nav_file = Path("D:\\_Investigations\\2021-03-19-taggingImages\\navigation.csv")
-input_directory = Path("D:\\_Investigations\\2021-03-19-taggingImages\\Colour-4ms-multipletargets-multipledepths hodgepodge")
-output_directory = Path("D:\\_Investigations\\2021-03-19-taggingImages\\Colour-4ms-multipletargets-multipledepths-hodgepodge-withDepth")
+depth_file = Path("D:\\_Investigations\\2021-03-24-NavDataExtract-ForImages\\depth.csv")
+nav_file = Path("D:\\_Investigations\\2021-03-24-NavDataExtract-ForImages\\nav.csv")
+input_directory = Path("D:\\_Investigations\\2021-03-24-NavDataExtract-ForImages\\NIGHT_5M_NANO_LED_4.8ms_3knotl")
+output_directory = Path("D:\\_Investigations\\2021-03-24-NavDataExtract-ForImages\\NIGHT_5M_NANO_LED_4.8ms_3knots-withGPS")
 
-datetime_offset = (dt.datetime(year=2020, month = 12, day = 10, hour = 1, minute = 40, second = 20) - dt.datetime.utcfromtimestamp(0)).total_seconds()
-print (datetime_offset)
+#datetime_offset = (dt.datetime(year=2020, month = 12, day = 10, hour = 1, minute = 40, second = 20) - dt.datetime.utcfromtimestamp(0)).total_seconds()
+#print (datetime_offset)
 
 if (not output_directory.exists()):
     output_directory.mkdir(exist_ok=True, parents=True)
@@ -28,10 +28,10 @@ for i, (in_file, out_file) in enumerate (zip(in_files, out_files)):
     if (not tags.load_header (str(in_file), error_message)):
         raise RuntimeError(error_message)
     
-    image_time = tags.pps_time/1.0E6 + datetime_offset
+    image_time = tags.pps_time/1.0E6 #+ datetime_offset
 
     time_index = np.searchsorted(depth[:, 0], image_time)
-    nav_index = np.searchsorted(nav[:, 0], image_time*1E6)
+    nav_index = np.searchsorted(nav[:, 0], tags.pps_time)
 
     tags.pps_time = int(image_time*1E6)
     tags.date_time = int(image_time*1E6)
@@ -44,9 +44,9 @@ for i, (in_file, out_file) in enumerate (zip(in_files, out_files)):
         tags.latitude = np.abs(lat)
         tags.longitude = np.abs(lon)
         tags.altitude = np.abs(altitude)
-        tags.latitudeRef = et.LATITUDEREF_NORTH if lat > 0 else et.LATITUDEREF_SOUTH
-        tags.latitudeRef = et.LONGITUDEREF_EAST if lon > 0 else et.LONGITUDEREF_WEST
-        tags.altitudeRef = et.ALTITUDEREF_ABOVE_SEA_LEVEL
+        tags.latitude_ref = et.LATITUDEREF_NORTH if lat > 0 else et.LATITUDEREF_SOUTH
+        tags.longitude_ref = et.LONGITUDEREF_EAST if lon > 0 else et.LONGITUDEREF_WEST
+        tags.altitude_ref = et.ALTITUDEREF_ABOVE_SEA_LEVEL
         et.save_tags (tags, str(in_file), str(out_file))
     except Exception as e:
         print (e)
