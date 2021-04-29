0a44f1b256a526127a5cfa36d3df986f97775d3c
Stuart MacDonald <smacdonald@2grobotics.com>
2021-04-29 09:40:57 -0400

    EXIFTags does not use conan, removed

--- build_release.sh
+++ build_release.sh
@@ -3,14 +3,6 @@
 
 set -e
 
-if [ -n "$JENKINS_HOME" ]; then
-    # This is required to avoid some subtle issue between Jenkins-as-SYSTEM using the Conan
-    # setup as installed by the tgrobotics user. Probably a permissions thing.
-    # Also Conan suggests that its cache is not suitable for concurrent access like a CI server
-    # produces, although that may be out of date. Haven't confirmed.
-    export CONAN_USER_HOME="$WORKSPACE"
-fi
-
 rm -rf build
 mkdir build
 cd build
