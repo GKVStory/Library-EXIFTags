89a6757bde83cb335ef48f4a5c917cd79ee0d32c
Stuart MacDonald <smacdonald@2grobotics.com>
2021-05-12 13:34:09 -0400

    "Disable" conan by restricting it to just TEST
    It's only required by test code and we're not building that in production, so
    this is an okay workaround. A future developer will have to `--build missing`
    to make opencv-2g match for this code.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -2,7 +2,3 @@ if (BUILD_PYTHON)
     include(PyBind11-Setup.cmake)
     include_directories(SYSTEM "${PYBIND11_INCLUDE_DIR}")
 endif()
-
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()
-set(CONAN_SYSTEM_INCLUDES ON)
