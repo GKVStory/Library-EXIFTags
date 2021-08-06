9fca2495f61a59cc5ed36d0f3086890542e94daf
Ryan Wicks <ryancwicks@gmail.com>
2021-08-06 12:22:22 -0400

    Removed make_unique for compatibility with old compilers.

--- src/Tag.cpp
+++ src/Tag.cpp
@@ -8,27 +8,27 @@ namespace tags {
 std::unique_ptr<Tag> Tag::tagFactory (const Constants::SupportedTags &  tag) {
     switch (Constants::TAG_INFO[tag].data_type) {
         case Constants::UINT32:
-            return std::make_unique<Tag_UINT32> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UINT32> (new Tag_UINT32(Constants::TAG_INFO[tag]));
         case Constants::UINT16:
-            return std::make_unique<Tag_UINT16> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UINT16> (new Tag_UINT16(Constants::TAG_INFO[tag]));
         case Constants::UINT8:
-            return std::make_unique<Tag_UINT8> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UINT8> (new Tag_UINT8(Constants::TAG_INFO[tag]));
         case Constants::UDOUBLE:
-            return std::make_unique<Tag_UDOUBLE> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UDOUBLE> (new Tag_UDOUBLE(Constants::TAG_INFO[tag]));
         case Constants::DOUBLE:
-            return std::make_unique<Tag_DOUBLE> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_DOUBLE> (new Tag_DOUBLE(Constants::TAG_INFO[tag]));
         case Constants::STRING:
-            return std::make_unique<Tag_STRING> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_STRING> (new Tag_STRING(Constants::TAG_INFO[tag]));
 		case Constants::UINT32_ARRAY:
-            return std::make_unique<Tag_UINT32_ARRAY> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UINT32_ARRAY> (new Tag_UINT32_ARRAY(Constants::TAG_INFO[tag]));
         case Constants::UINT16_ARRAY:
-            return std::make_unique<Tag_UINT16_ARRAY> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UINT16_ARRAY> (new Tag_UINT16_ARRAY(Constants::TAG_INFO[tag]));
 		case Constants::UINT8_ARRAY:
-            return std::make_unique<Tag_UINT8_ARRAY> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UINT8_ARRAY> (new Tag_UINT8_ARRAY(Constants::TAG_INFO[tag]));
         case Constants::DOUBLE_ARRAY:
-            return std::make_unique<Tag_DOUBLE_ARRAY> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_DOUBLE_ARRAY> (new Tag_DOUBLE_ARRAY(Constants::TAG_INFO[tag]));
         case Constants::UDOUBLE_ARRAY:
-            return std::make_unique<Tag_UDOUBLE_ARRAY> (Constants::TAG_INFO[tag]);
+            return std::unique_ptr<Tag_UDOUBLE_ARRAY> (new Tag_UDOUBLE_ARRAY(Constants::TAG_INFO[tag]));
         default:
             return nullptr;
     }
